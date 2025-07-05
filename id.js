450733ae38318f1adfb00b776e9ce69c2c1ab7e6
LMK360 <lmkfail080@gmail.com>
2025-07-05 19:30:42 +0200

    Update id.js

--- id.js
+++ id.js
@@ -1,10 +1,14 @@
-function makeid(num = 4) {
-  let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var characters9 = characters.length;
-  for (var i = 0; i < num; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters9));
-  }
-  return result;
+function makeSessionId() {
+  const part = (len) => {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    let result = "";
+    for (let i = 0; i < len; i++) {
+      result += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return result;
+  };
+
+  return `LMK~part(8)#{part(16)}-part(20)-{part(24)}`;
 }
-module.exports = {makeid};
+
+module.exports = { makeid };
